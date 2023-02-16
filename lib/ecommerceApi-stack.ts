912392401b8cbd38b567518d6c3ac9a2676d7772
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-02-16 13:18:13 -0300

    feat(apigateway): sync api gateway with lambda func

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -5,12 +5,25 @@ import * as cwlogs from 'aws-cdk-lib/aws-logs'
 
 import { Construct } from 'constructs'
 
-export class ECommerceApiStaco extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface ECommerceApiStackProps extends cdk.StackProps {
+  productsFetchHandler: lambdaNodeJS.NodejsFunction
+}
+
+export class ECommerceApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
     super(scope, id, props)
 
     const api = new apigateway.RestApi(this, 'ECommerceApi', {
       restApiName: 'ECommerceApi',
     })
+    //Conectando a integracao do API Gateway com o nosso Lambda!
+    const productsFetchIntegration = new apigateway.LambdaIntegration(
+      props.productsFetchHandler
+    )
+
+    //criar o recurso de produttos -> o / tem sua representacao pelo root  /products
+    const productsResource = api.root.addResource('products')
+    //criando o metodo
+    productsResource.addMethod('GET', productsFetchIntegration)
   }
 }
