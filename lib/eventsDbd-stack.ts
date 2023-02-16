ef2ecdcac246ebc32104966c9cc752944e70119e
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-02-16 20:06:27 -0300

    feat(events): creating events table stack

---
+++ lib/eventsDbd-stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+
+import { Construct } from 'constructs'
+
+export class EventsDbdStack extends cdk.Stack {
+  readonly table: dynamodb.Table
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+
+    this.table = new dynamodb.Table(this, 'EventsDbd', {
+      tableName: 'events',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: 'pk',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'sk',
+        type: dynamodb.AttributeType.STRING,
+      },
+      timeToLiveAttribute: 'ttl',
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    })
+  }
+}
