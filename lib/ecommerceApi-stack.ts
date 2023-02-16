81b1a8ebb727692ca61bfac98de42d92cba4600e
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-02-16 13:22:51 -0300

    feat(apigateway): creating logs in cloud watch

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -12,9 +12,27 @@ interface ECommerceApiStackProps extends cdk.StackProps {
 export class ECommerceApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
     super(scope, id, props)
-
+    //Criando os Logs para visualizar o API Gateway
+    const logGroup = new cwlogs.LogGroup(this, 'ECommerceApiLogs')
     const api = new apigateway.RestApi(this, 'ECommerceApi', {
       restApiName: 'ECommerceApi',
+      deployOptions: {
+        //Inserindo onde o Cloudwatch vai criar o grupo de logs. Os logs sao para verificar quais informacoes acessam nossos lambdas
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        //Alterando o formato do log que vai aparecer no CloudWatch e atributos que vao aparecer no log
+        //Esses logs vao cobrar sempre e alem de retornar dados sensiveis ip e usuario...
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true,
+        }),
+      },
     })
     //Conectando a integracao do API Gateway com o nosso Lambda!
     const productsFetchIntegration = new apigateway.LambdaIntegration(
