7574a137fd6175ca7d7aef25925f8f53c5d89237
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-02-20 19:18:15 -0300

    feat(lambda): creating new lambda to products handle

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -8,12 +8,16 @@ import * as ssm from 'aws-cdk-lib/aws-ssm'
 
 import { Construct } from 'constructs'
 
+interface ProductsAppStackProps extends cdk.StackProps {
+  eventsDbd: dynamodb.Table
+}
+
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
   readonly productsAdminHandler: lambdaNodeJS.NodejsFunction
   readonly productsDdb: dynamodb.Table
   //scope = todo projeto do cdk. id = id propriamente dito do projeto. props = propriedades passadas para o CDK
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ProductsAppStackProps) {
     super(scope, id, props)
     //criando tabela no banco dynamo
     this.productsDdb = new dynamodb.Table(this, 'ProductsDbd', {
@@ -38,6 +42,28 @@ export class ProductsAppStack extends cdk.Stack {
       this,
       'ProductsLayerVersionArn',
       productsLayerArn
+    )
+    //Criando funcao que sera chamada em outro lambda
+    const productEventsHandler = new lambdaNodeJS.NodejsFunction(
+      this,
+      'ProductsEventsFunction',
+      {
+        functionName: 'ProductsEventsFunction',
+        entry: 'lambda/products/ProductsEventsFunction.ts',
+        handler: 'handler',
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(2),
+        bundling: {
+          //como vamos empacotar o arquivo e subir na AWS.
+          minify: true,
+          sourceMap: false,
+        },
+        environment: {
+          EVENTS_DDB: props.eventsDbd.tableName,
+        },
+        tracing: lambda.Tracing.ACTIVE,
+        insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0,
+      }
     )
 
     //configurando como sera minha funcao lambda
