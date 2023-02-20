5fe0db6eb9e41a0cea81d9635c7a9991c7a30e96
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-02-20 19:24:50 -0300

    feat(layer): creating new layer

--- lib/productsAppLayers-stack.ts
+++ lib/productsAppLayers-stack.ts
@@ -5,12 +5,10 @@ import * as ssm from 'aws-cdk-lib/aws-ssm'
 import { Construct } from 'constructs'
 
 export class ProductsAppLayersStack extends cdk.Stack {
-  readonly productsLayers: lambda.LayerVersion
-
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    this.productsLayers = new lambda.LayerVersion(this, 'ProductsLayer', {
+    const productsLayers = new lambda.LayerVersion(this, 'ProductsLayer', {
       code: lambda.Code.fromAsset('lambda/products/layers/productsLayer'),
       compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
       layerVersionName: 'ProductsLayer',
@@ -18,7 +16,24 @@ export class ProductsAppLayersStack extends cdk.Stack {
     })
     new ssm.StringParameter(this, 'ProductsLayerVersionArn', {
       parameterName: 'ProductsLayerVersionArn',
-      stringValue: this.productsLayers.layerVersionArn,
+      stringValue: productsLayers.layerVersionArn,
+    })
+
+    const ProductEventsLayer = new lambda.LayerVersion(
+      this,
+      'ProductEventsLayer',
+      {
+        code: lambda.Code.fromAsset(
+          'lambda/products/layers/productEventsLayer'
+        ),
+        compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
+        layerVersionName: 'ProductEventsLayer',
+        removalPolicy: cdk.RemovalPolicy.RETAIN,
+      }
+    )
+    new ssm.StringParameter(this, 'ProductEventsLayerVersionArn', {
+      parameterName: 'ProductEventsLayerVersionArn',
+      stringValue: ProductEventsLayer.layerVersionArn,
     })
   }
 }
