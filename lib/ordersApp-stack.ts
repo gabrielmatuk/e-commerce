5d3888f485c58352b177420ceac404d90c1d92ea
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-03-02 23:03:01 -0300

    feat(iam): creating event policy statement

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -5,6 +5,7 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import * as ssm from 'aws-cdk-lib/aws-ssm'
 import * as sns from 'aws-cdk-lib/aws-sns'
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions'
+import * as iam from 'aws-cdk-lib/aws-iam'
 
 import { Construct } from 'constructs'
 
@@ -139,5 +140,18 @@ export class OrdersAppStack extends cdk.Stack {
     )
 
     ordersTopic.addSubscription(new subs.LambdaSubscription(orderEventsHandler))
+
+    const eventsDdbPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['dynamodb:PutItem'],
+      resources: [props.eventsDbd.tableArn],
+      conditions: {
+        ['ForAllValues:StringLike']: {
+          'dynamodb:LeadingKeys': ['#order_*'],
+        },
+      },
+    })
+
+    orderEventsHandler.addToRolePolicy(eventsDdbPolicy)
   }
 }
