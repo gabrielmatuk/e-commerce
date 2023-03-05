3374d1e1d313adbe6eefabbbe8acd753897df850
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-03-05 18:36:12 -0300

    feat(eventsOrder): creating new route in api gateway

--- bin/ecommerce.ts
+++ bin/ecommerce.ts
@@ -68,6 +68,7 @@ const eCommerceApiStack = new ECommerceApiStack(app, 'ECommerceApi', {
   productsFetchHandler: productsAppStack.productsFetchHandler,
   productsAdminHandler: productsAppStack.productsAdminHandler,
   ordersHandler: ordersAppStack.ordersHandler,
+  orderEventsFetchHandler: ordersAppStack.orderEventsFetchHandler,
   tags: tags,
   env: env,
 })
