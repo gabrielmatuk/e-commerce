3d0c1f0433c901a31fa84296d1dfd32e802a20ba
Gabriel Matuk <gabrielmatuk14@gmail.com>
2023-03-05 22:17:15 -0300

    feat(invoices): add get url function:

---
+++ lambda/invoices/invoiceGetUrlFunction.ts
@@ -0,0 +1,46 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from 'aws-lambda'
+import { ApiGatewayManagementApi, DynamoDB, S3 } from 'aws-sdk'
+import { v4 as uuid } from 'uuid'
+import * as AWSXray from 'aws-xray-sdk'
+
+AWSXray.captureAWS(require('aws-sdk'))
+
+const invoicesDdb = process.env.INVOICE_DDB!
+const bucketName = process.env.BUCKET_NAME!
+const invoicesWsApiEndpoint = process.env.INVOICE_WSAPI_ENDPOINT!
+
+const s3Client = new S3()
+const ddbClient = new DynamoDB.DocumentClient()
+const apigwManagementApi = new ApiGatewayManagementApi({
+  endpoint: invoicesWsApiEndpoint,
+})
+
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  const lambdaRequestId = context.awsRequestId
+  const connectionId = event.requestContext.connectionId!
+
+  console.log(
+    `ConnectionId: ${connectionId} - Lambda RequestId: ${lambdaRequestId}`
+  )
+
+  const key = uuid()
+  const expires = 300
+
+  const signedUrl = await s3Client.getSignedUrlPromise('putObject', {
+    Bucket: bucketName,
+    Key: key,
+    Expires: expires,
+  })
+
+  return {
+    statusCode: 200,
+    body: 'OK!',
+  }
+}
